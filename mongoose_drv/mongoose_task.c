6632a7bbc580bd3e6258b3e821ee9817863ea5fc
szevczenko <dima.szevczenko@gmail.com>
2024-12-23 08:16:34 +0100

    Separate mongoose task

---
+++ mongoose_drv/mongoose_task.c
@@ -0,0 +1,32 @@
+#include "mongoose_task.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "mongoose.h"
+
+struct mg_mgr mgr;
+static TaskHandle_t mongooseTaskHandle = NULL;
+
+static void _task( void* pvParameters )
+{
+  while ( 1 )
+  {
+    mg_mgr_poll( &mgr, 1000 );    // Poll mongoose manager every 1000 ms
+  }
+}
+
+void MongooseTask_Init( void )
+{
+  mg_mgr_init( &mgr );
+  xTaskCreate( _task, "mg_poll", 2048, NULL, 5, &mongooseTaskHandle );
+}
+
+void MongooseTask_Deinit( void )
+{
+  if ( mongooseTaskHandle != NULL )
+  {
+    vTaskDelete( mongooseTaskHandle );
+    mongooseTaskHandle = NULL;
+  }
+  mg_mgr_free( &mgr );
+}
