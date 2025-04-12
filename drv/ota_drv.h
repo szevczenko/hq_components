97d8d48df321da3b3bb076059b11de2e97040aed
szevczenko <dima.szevczenko@gmail.com>
2025-04-12 15:32:37 +0200

    Rework ota driver

--- drv/ota_drv.h
+++ drv/ota_drv.h
@@ -18,19 +18,20 @@
 
 typedef enum
 {
-  OTA_DRIVER_STATE_IDLE,
-  OTA_DRIVER_STATE_DOWNLOAD,
-  OTA_DRIVER_STATE_ERROR,
-  OTA_DRIVER_STATE_DOWNLOAD_FINISHED,
-  OTA_DERIVER_LAST
-} ota_driver_state_t;
+  OTA_DRV_OK,
+  OTA_DRV_FAIL,
+  OTA_DRV_APP_VALID_AFTER_UPDATE
+} ota_drv_status_t;
 
 /* Public functions ----------------------------------------------------------*/
 
 /**
  * @brief   Init ota driver.
+ * @return  OTA_DRV_OK if initialization is successful,
+ *          OTA_DRV_APP_VALID_AFTER_UPDATE if app is valid after update,
+ *          OTA_DRV_FAIL if initialization fails.
  */
-void OTA_Init( void );
+ota_drv_status_t OTA_Init( void );
 
 /**
  * @brief   Download image.
@@ -38,13 +39,30 @@ void OTA_Init( void );
 bool OTA_Download( const char* url );
 
 /**
- * @brief   Get OTA state.
+ * @brief   Get the OTA initialization status.
+ * @return  The status of the OTA initialization.
  */
-ota_driver_state_t OTA_GetState( void );
+ota_drv_status_t OTA_GetStatus( void );
 
 /**
- * @brief   Get OTA downloaded percentage.
+ * @brief   Begin OTA update.
+ * @param   new_firmware_size - Size of the new firmware.
+ * @return  true if successful, false otherwise.
  */
-size_t OTA_GetDownloadPercentage( void );
+bool OTA_Begin( size_t new_firmware_size );
+
+/**
+ * @brief   Write OTA data.
+ * @param   buf - Buffer containing the data to write.
+ * @param   len - Length of the data to write.
+ * @return  true if successful, false otherwise.
+ */
+bool OTA_Write( const void* buf, size_t len );
+
+/**
+ * @brief   End OTA update.
+ * @return  true if successful, false otherwise.
+ */
+bool OTA_End( void );
 
 #endif
